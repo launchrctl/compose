6255153a237b99cd71b6b088adeffbe46741ad5b
Aleksandr Britvin <lexbritvin@gmail.com>
2024-12-22 22:05:29 +0100

    Refactor to new actions.

--- plugin.go
+++ plugin.go
@@ -2,16 +2,30 @@
 package compose
 
 import (
+	"context"
+	_ "embed"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/launchrctl/keyring"
+
 	"github.com/launchrctl/launchr"
 	"github.com/launchrctl/launchr/pkg/action"
 
 	"github.com/launchrctl/compose/compose"
+)
+
+var (
+	//go:embed action.compose.yaml
+	actionComposeYaml []byte
+	//go:embed action.add.yaml
+	actionAddYaml []byte
+	//go:embed action.update.yaml
+	actionUpdateYaml []byte
+	//go:embed action.delete.yaml
+	actionDeleteYaml []byte
 )
 
 func init() {
@@ -38,146 +52,108 @@ func (p *Plugin) OnAppInit(app launchr.App) error {
 	return nil
 }
 
-// CobraAddCommands implements [launchr.CobraPlugin] interface to provide compose functionality.
-func (p *Plugin) CobraAddCommands(rootCmd *launchr.Command) error {
-	var workingDir string
-	var skipNotVersioned bool
-	var conflictsVerbosity bool
-	var clean bool
-	var interactive bool
+// DiscoverActions implements [launchr.ActionDiscoveryPlugin] interface.
+func (p *Plugin) DiscoverActions(_ context.Context) ([]*action.Action, error) {
+	// Action compose.
+	composeAction := action.NewFromYAML("compose", actionComposeYaml)
+	composeAction.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
+		input := a.Input()
+		c, err := compose.CreateComposer(
+			p.wd,
+			compose.ComposerOptions{
+				Clean:              input.Opt("clean").(bool),
+				WorkingDir:         input.Opt("working-dir").(string),
+				SkipNotVersioned:   input.Opt("skip-not-versioned").(bool),
+				ConflictsVerbosity: input.Opt("conflicts-verbosity").(bool),
+				Interactive:        input.Opt("interactive").(bool),
+			},
+			p.k,
+		)
+		if err != nil {
+			return err
+		}
 
-	var composeCmd = &launchr.Command{
-		Use:   "compose",
-		Short: "Composes filesystem (files & dirs)",
-		RunE: func(cmd *launchr.Command, _ []string) error {
-			// Don't show usage help on a runtime error.
-			cmd.SilenceUsage = true
+		return c.RunInstall()
+	}))
 
-			c, err := compose.CreateComposer(
-				p.wd,
-				compose.ComposerOptions{
-					Clean:              clean,
-					WorkingDir:         workingDir,
-					SkipNotVersioned:   skipNotVersioned,
-					ConflictsVerbosity: conflictsVerbosity,
-					Interactive:        interactive,
-				},
-				p.k,
-			)
-			if err != nil {
-				return err
-			}
+	// Action compose:add.
+	addAction := action.NewFromYAML("compose:add", actionAddYaml)
+	addAction.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
+		input := a.Input()
+		if err := packagePreRunValidate(input); err != nil {
+			return err
+		}
+		createNew := input.Opt("allow-create").(bool)
+		composeDependency := getInputDependencies(input)
+		strategies := getInputStrategies(input)
+		return compose.AddPackage(createNew, composeDependency, strategies, p.wd)
+	}))
 
-			return c.RunInstall()
-		},
-	}
+	// Action compose:update.
+	updateAction := action.NewFromYAML("compose:update", actionUpdateYaml)
+	updateAction.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
+		input := a.Input()
+		if err := packagePreRunValidate(input); err != nil {
+			return err
+		}
+		composeDependency := getInputDependencies(input)
+		strategies := getInputStrategies(input)
+		if composeDependency.Name != "" {
+			return compose.UpdatePackage(composeDependency, strategies, p.wd)
+		}
 
-	composeCmd.Flags().StringVarP(&workingDir, "working-dir", "w", ".compose/packages", "Working directory for temp files")
-	composeCmd.Flags().BoolVarP(&skipNotVersioned, "skip-not-versioned", "s", false, "Skip not versioned files from source directory (git only)")
-	composeCmd.Flags().BoolVar(&conflictsVerbosity, "conflicts-verbosity", false, "Log files conflicts")
-	composeCmd.Flags().BoolVar(&clean, "clean", false, "Remove .compose dir on start")
-	composeCmd.Flags().BoolVar(&interactive, "interactive", true, "Interactive mode allows to submit user credentials during action")
+		return compose.UpdatePackages(p.wd)
+	}))
 
-	composeDependency := &compose.Dependency{}
-	strategies := &compose.RawStrategies{}
-	var createNew bool
-	var addCmd = &launchr.Command{
-		Use:   "compose:add",
-		Short: "Add a new package to plasma-compose",
-		PreRunE: func(cmd *launchr.Command, args []string) error {
-			return packagePreRunValidate(cmd, args)
-		},
-		RunE: func(cmd *launchr.Command, _ []string) error {
-			// Don't show usage help on a runtime error.
-			cmd.SilenceUsage = true
+	// Action compose:delete.
+	deleteAction := action.NewFromYAML("compose:delete", actionDeleteYaml)
+	deleteAction.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
+		input := a.Input()
+		toDeletePackages := action.InputOptSlice[string](input, "packages")
+		return compose.DeletePackages(toDeletePackages, p.wd)
+	}))
 
-			return compose.AddPackage(createNew, composeDependency, strategies, p.wd)
-		},
-	}
-
-	var updateCmd = &launchr.Command{
-		Use:   "compose:update",
-		Short: "Update a plasma-compose package",
-		PreRunE: func(cmd *launchr.Command, args []string) error {
-			return packagePreRunValidate(cmd, args)
-		},
-		RunE: func(cmd *launchr.Command, _ []string) error {
-			// Don't show usage help on a runtime error.
-			cmd.SilenceUsage = true
-
-			if composeDependency.Name != "" {
-				return compose.UpdatePackage(composeDependency, strategies, p.wd)
-			}
-
-			return compose.UpdatePackages(p.wd)
-		},
-	}
-
-	var toDeletePackages []string
-	var deleteCmd = &launchr.Command{
-		Use:   "compose:delete",
-		Short: "Remove a package from plasma-compose",
-		RunE: func(cmd *launchr.Command, _ []string) error {
-			// Don't show usage help on a runtime error.
-			cmd.SilenceUsage = true
-
-			return compose.DeletePackages(toDeletePackages, p.wd)
-		},
-	}
-
-	addCmd.Flags().BoolVarP(&createNew, "allow-create", "", false, "Create plasma-compose if not exist")
-	addPackageFlags(addCmd, composeDependency, strategies)
-	addPackageFlags(updateCmd, composeDependency, strategies)
-
-	deleteCmd.Flags().StringSliceVarP(&toDeletePackages, "packages", "", []string{}, "List of packages to remove. Comma separated.")
-
-	rootCmd.AddCommand(composeCmd)
-	rootCmd.AddCommand(addCmd)
-	rootCmd.AddCommand(updateCmd)
-	rootCmd.AddCommand(deleteCmd)
-
-	return nil
+	return []*action.Action{
+		composeAction,
+		addAction,
+		updateAction,
+		deleteAction,
+	}, nil
 }
 
-func addPackageFlags(cmd *launchr.Command, dependency *compose.Dependency, strategies *compose.RawStrategies) {
-	cmd.Flags().StringVarP(&dependency.Name, "package", "", "", "Name of the package")
-	compose.EnumVarP(cmd, &dependency.Source.Type, "type", "", compose.GitType, []string{compose.GitType, compose.HTTPType}, "Type of the package source: git, http")
-	cmd.Flags().StringVarP(&dependency.Source.Ref, "ref", "", "", "Reference of the package source")
-	cmd.Flags().StringVarP(&dependency.Source.URL, "url", "", "", "URL of the package source")
-
-	cmd.Flags().StringSliceVarP(&strategies.Names, "strategy", "", []string{}, "Strategy name")
-	cmd.Flags().StringSliceVarP(&strategies.Paths, "strategy-path", "", []string{}, "Strategy paths. paths separated by |, strategies are comma separated (path/1|path/2,path/1|path/2)")
+func getInputDependencies(input *action.Input) *compose.Dependency {
+	return &compose.Dependency{
+		Name: input.Opt("package").(string),
+		Source: compose.Source{
+			Type: input.Opt("type").(string),
+			Ref:  input.Opt("ref").(string),
+			Tag:  input.Opt("tag").(string),
+			URL:  input.Opt("url").(string),
+		},
+	}
 }
 
-func packagePreRunValidate(cmd *launchr.Command, _ []string) error {
-	typeFlag, err := cmd.Flags().GetString("type")
-	if err != nil {
-		return err
+func getInputStrategies(input *action.Input) *compose.RawStrategies {
+	return &compose.RawStrategies{
+		Names: action.InputOptSlice[string](input, "strategy"),
+		Paths: action.InputOptSlice[string](input, "strategy-path"),
 	}
+}
+
+func packagePreRunValidate(input *action.Input) error {
+	typeFlag := input.Opt("type").(string)
 
 	if typeFlag == compose.HTTPType {
-		if cmd.Flag("ref").Changed {
+		refChanged := input.Opt("ref").(string) != ""
+		if refChanged {
 			launchr.Term().Warning().Println("Ref can't be used with HTTP source")
-			err = cmd.Flags().Set("ref", "")
+			input.SetOpt("ref", "")
 		}
 	}
 
-	strategyChanged := cmd.Flag("strategy").Changed
-	pathsChanged := cmd.Flag("strategy-path").Changed
-	if strategyChanged || pathsChanged {
-		var strategies []string
-		var paths []string
-
-		strategies, err = cmd.Flags().GetStringSlice("strategy")
-		if err != nil {
-			return err
-		}
-
-		paths, err = cmd.Flags().GetStringSlice("strategy-path")
-		if err != nil {
-			return err
-		}
-
+	strategies := action.InputOptSlice[string](input, "strategy")
+	paths := action.InputOptSlice[string](input, "strategy-path")
+	if len(strategies) > 0 || len(paths) > 0 {
 		if len(strategies) != len(paths) {
 			return errors.New("number of strategies and paths must be equal")
 		}
@@ -196,5 +172,5 @@ func packagePreRunValidate(cmd *launchr.Command, _ []string) error {
 		}
 	}
 
-	return err
+	return nil
 }
